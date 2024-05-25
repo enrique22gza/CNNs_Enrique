0520502be041d32b573fa917f00cbee854da0430
enrique22gza <79334059+enrique22gza@users.noreply.github.com>
2024-05-25 12:57:05 +0200

    Update inceptionV3.py

--- inceptionV3.py
+++ inceptionV3.py
@@ -2,7 +2,7 @@ import os
 import numpy as np
 import pandas as pd
 import tensorflow as tf
-from tensorflow.keras.applications import InceptionV3
+from tensorflow.keras.applications import DenseNet121, InceptionV3
 from tensorflow.keras.models import Model
 from tensorflow.keras.layers import Dense, Dropout, GlobalAveragePooling2D
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
@@ -41,7 +41,7 @@ data = pd.DataFrame({
 datagen = ImageDataGenerator(rescale=1./255)
 
 # Crear el modelo InceptionV3
-def create_model():
+def create_inception_model():
     base_model = InceptionV3(weights='imagenet', include_top=False, input_shape=(img_height, img_width, 3))
     x = base_model.output
     x = GlobalAveragePooling2D()(x)
@@ -60,10 +60,10 @@ def create_model():
 early_stopping = EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True)
 reduce_lr = ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=3, min_lr=1e-6)
 
-# Cross-validation
+# Cross-validation para InceptionV3
 kf = StratifiedKFold(n_splits=num_folds, shuffle=True, random_state=42)
 fold_no = 1
-results = []
+results_inception = []
 
 for train_index, val_index in kf.split(data['filename'], data['class']):
     print(f'Fold {fold_no}')
@@ -91,21 +91,21 @@ for train_index, val_index in kf.split(data['filename'], data['class']):
         shuffle=True
     )
     
-    model = create_model()
+    model_inception = create_inception_model()
     
-    model_checkpoint = ModelCheckpoint(f'model_fold_{fold_no}.keras', save_best_only=True, monitor='val_loss')
+    model_checkpoint = ModelCheckpoint(f'inception_model_fold_{fold_no}.keras', save_best_only=True, monitor='val_loss')
     
-    history = model.fit(
+    history = model_inception.fit(
         train_gen_split,
         validation_data=val_gen_split,
         epochs=epochs,
         callbacks=[early_stopping, reduce_lr, model_checkpoint]
     )
     
-    results.append(model.evaluate(val_gen_split))
+    results_inception.append(model_inception.evaluate(val_gen_split))
     fold_no += 1
 
 # Resultados
-print(f'Resultados de {num_folds}-Fold Cross Validation:')
-for i, result in enumerate(results):
+print(f'Resultados de {num_folds}-Fold Cross Validation para InceptionV3:')
+for i, result in enumerate(results_inception):
     print(f'Fold {i+1}: Loss = {result[0]}, Accuracy = {result[1]}')
